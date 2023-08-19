5866ae7abe3bd8b3a6d813e565970b43e8f607ff
Alex Ulanch <alexulanch@gmail.com>
2023-08-19 18:50:53 -0400

    Initial Commit

---
+++ Guide Book/Guide_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guide_BookApp.swift
+//  Guide Book
+//
+//  Created by Alex Ulanch on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Guide_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
